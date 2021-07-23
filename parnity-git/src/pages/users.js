78b889cd89230b1bc12afec71896e2431b1176ec
Lucas <lucas_221910@hotmail.com>
2021-07-23 13:15:48 -0300

    Add Users.js Add Users.css

---
+++ parnity-git/src/pages/users.js
@@ -0,0 +1,46 @@
+import Head from "next/head"
+import { Footer } from "../components/Footer"
+import { Header } from "../components/Header"
+import styles from "../styles/pages/Usuarios.module.css"
+
+
+export const getStaticProps = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const json = await res.json();
+
+    return {
+        props: { data: json }
+    }
+}
+
+
+export default function Usuarios({ data }) {
+    return (
+        <div className={styles.container}>
+
+            <Head>
+                <title>Usuários</title>
+            </Head>
+
+            <Header />
+
+            <div className={styles.mainFooterContainer}>
+                <main className={styles.main}>
+                    {data.map(data => (
+                        <div className={styles.card} key={data.id}>
+                            {data.username}
+                        </div>
+                    ))}
+                    {data.map(data => (
+                        <div className={styles.card} key={data.id}>
+                            {data.username}
+                        </div>
+                    ))}
+                </main>
+
+
+                <Footer />
+            </div>
+        </div>
+    )
+}
