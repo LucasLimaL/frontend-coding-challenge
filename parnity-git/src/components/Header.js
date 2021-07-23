55dc3423dec0054be8c86543c893c588d4bc163e
Lucas <lucas_221910@hotmail.com>
2021-07-23 13:09:36 -0300

    Add content

---
+++ parnity-git/src/components/Header.js
@@ -0,0 +1,9 @@
+import styles from '../styles/components/Header.module.css'
+
+export function Header() {
+    return (
+        <header className={styles.Header}>
+            Header
+        </header>
+    )
+}
