f8e2ffbfdd2e34914c969372c3df66731ea08caa
Lucas <lucas_221910@hotmail.com>
2021-07-23 13:10:13 -0300

    Add Footer content

---
+++ parnity-git/src/components/Footer.js
@@ -0,0 +1,9 @@
+import styles from '../styles/components/Footer.module.css'
+
+export function Footer() {
+    return (
+        <footer className={styles.Footer}>
+            <span>copyright</span>
+        </footer>
+    )
+}
