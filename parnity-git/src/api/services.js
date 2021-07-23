4d1a7580b53eef0b85e29b43fb1593e6437b5f4c
Lucas <lucas_221910@hotmail.com>
2021-07-23 13:22:55 -0300

    Add services.js

---
+++ parnity-git/src/api/services.js
@@ -0,0 +1,8 @@
+// export const getStaticProps = async () => {
+//     const res = await fetch('https://jsonplaceholder.typicode.com/users')
+//     const json = await res.json();
+
+//     return {
+//         props: { data: json }
+//     }
+// }
